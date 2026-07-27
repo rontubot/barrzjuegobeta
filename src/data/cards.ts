4767ebf3a33856a88330fc6996640796d92e9696
rontubot <rontubot@gmail.com>
2026-07-27 15:32:07 -0300

    feat: link BEATS_DECK in cards.ts to the new local WAV beats inside public soundtracks

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -24,67 +24,67 @@ export interface ChallengeCard {
 export const BEATS_DECK: BeatCard[] = [
   {
     id: 'beat-1',
-    name: 'RUIDA 86',
-    bpm: 86,
+    name: 'MODO DIABLO 94',
+    bpm: 94,
     spotifyUrl: 'https://open.spotify.com/track/4PTG3Z6ehGkBF3sI7Wq2Ag',
     spotifyUri: 'spotify:track:4PTG3Z6ehGkBF3sI7Wq2Ag',
-    audioUrl: '/soundtracks/back.mpeg'
+    audioUrl: '/soundtracks/BEATS/MODO DIABLO 94.wav'
   },
   {
     id: 'beat-2',
-    name: 'CITRICO 93',
-    bpm: 93,
+    name: 'RUIDA 87',
+    bpm: 87,
     spotifyUrl: 'https://open.spotify.com/track/17dZsnvjQkXUWh6Sifg6hV',
     spotifyUri: 'spotify:track:17dZsnvjQkXUWh6Sifg6hV',
-    audioUrl: '/soundtracks/desconocidos.mpeg'
+    audioUrl: '/soundtracks/BEATS/RUIDA 87.wav'
   },
   {
     id: 'beat-3',
-    name: 'CHOCOLATADA 91',
-    bpm: 91,
+    name: 'TRAKETERO 88',
+    bpm: 88,
     spotifyUrl: 'https://open.spotify.com/track/5QdA5bEwS1hRzWbTep7XjO',
     spotifyUri: 'spotify:track:5QdA5bEwS1hRzWbTep7XjO',
-    audioUrl: '/soundtracks/electric-try 3.mp3.mpeg'
+    audioUrl: '/soundtracks/BEATS/TRAKETERO 88.wav'
   },
   {
     id: 'beat-4',
-    name: 'RAMEN 89',
-    bpm: 89,
+    name: 'BEAT 6 120 BPM',
+    bpm: 120,
     spotifyUrl: 'https://open.spotify.com/track/27O7B27xW3g3G0c4xX4Xp3',
     spotifyUri: 'spotify:track:27O7B27xW3g3G0c4xX4Xp3',
-    audioUrl: '/soundtracks/ovni 2.mp3.mpeg'
+    audioUrl: '/soundtracks/BEATS/BEAT 6 120 BPM.wav'
   },
   {
     id: 'beat-5',
-    name: 'PAN CASERO 91',
-    bpm: 91,
+    name: 'BEAT 7 110 BPM',
+    bpm: 110,
     spotifyUrl: 'https://open.spotify.com/track/3zHn2l3j8RSpYyGf9H2D3B',
     spotifyUri: 'spotify:track:3zHn2l3j8RSpYyGf9H2D3B',
-    audioUrl: '/soundtracks/back.mpeg'
+    audioUrl: '/soundtracks/BEATS/BEAT 7 110 BPM.wav'
   },
   {
     id: 'beat-6',
-    name: 'ESPEJO 87',
-    bpm: 87,
+    name: 'BEAT 8 96 BPM',
+    bpm: 96,
     spotifyUrl: 'https://open.spotify.com/track/4F4B3z4c1D2e3f4g5h6i7j',
     spotifyUri: 'spotify:track:4F4B3z4c1D2e3f4g5h6i7j',
-    audioUrl: '/soundtracks/desconocidos.mpeg'
+    audioUrl: '/soundtracks/BEATS/BEAT 8 96 BPM.wav'
   },
   {
     id: 'beat-7',
-    name: 'LA REINA 152',
-    bpm: 152,
+    name: 'BEAT 9 104 BPM',
+    bpm: 104,
     spotifyUrl: 'https://open.spotify.com/track/5G5H5i5j5k5l5m5n5o5p5q',
     spotifyUri: 'spotify:track:5G5H5i5j5k5l5m5n5o5p5q',
-    audioUrl: '/soundtracks/electric-try 3.mp3.mpeg'
+    audioUrl: '/soundtracks/BEATS/BEAT 9 104 BPM.wav'
   },
   {
     id: 'beat-8',
-    name: 'PULSO 120',
-    bpm: 120,
+    name: 'BEAT 11 92 BPM',
+    bpm: 92,
     spotifyUrl: 'https://open.spotify.com/track/6Q6R6s6t6u6v6w6x6y6z6A',
     spotifyUri: 'spotify:track:6Q6R6s6t6u6v6w6x6y6z6A',
-    audioUrl: '/soundtracks/ovni 2.mp3.mpeg'
+    audioUrl: '/soundtracks/BEATS/BEAT 11 92 BPM.wav'
   }
 ];
 
